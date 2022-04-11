b9597640c1151e5aaf40b73febb826aec2e8b0c6
NotDayga <92517382+NotDayga@users.noreply.github.com>
2022-04-11 00:23:28 -0700

    Update Book.java

--- Library Management System/src/Book.java
+++ Library Management System/src/Book.java
@@ -1,17 +1,59 @@
+import javax.naming.NamingEnumeration;
+
 public class Book {
     private String name;
     private String type;
     private int countID = 0;
     private int countOfBooks = 0;
-    private String names[];
-    private String types[];
+    private String names[] = new String[this.countOfBooks+1];
+    private String types[] = new String[this.countOfBooks+1];
 
-    public void givingBookID(String name,String type){
+    public Book(String name,String type){
         this.name = name;
         this.type = type;
-        String names[] = new String[this.countOfBooks+1];
-        String types[] = new String[this.countOfBooks+1];
-        for (int i = 0; i < names.length; i++) {
+    }
+
+    public String getType() {
+        return type;
+    }
+    public int getCountID() {
+        return countID;
+    }
+    public int getCountOfBooks() {
+        return countOfBooks;
+    }
+    public String getName() {
+        return name;
+    }
+    public String[] getNames() {
+        return names;
+    }
+    public String[] getTypes() {
+        return types;
+    }
+    
+    public void setCountID(int countID) {
+        this.countID = countID;
+    }
+    public void setCountOfBooks(int countOfBooks) {
+        this.countOfBooks = countOfBooks;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setNames(String[] names) {
+        this.names = names;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setTypes(String[] types) {
+        this.types = types;
+    }
+    
+    public void givingBookID(Book book){
+        book = new Book(this.name,this.type);
+        for (int i = 0; i < this.names.length; i++) {
             if (names[i].compareTo("")== 0) {
                 this.names[i] = this.name;
                 this.types[i] = this.type;
@@ -32,6 +74,7 @@ public class Book {
                 System.out.println("You found "+this.names[i]+ "which is a "+this.types[i]+" with the identification number:"+i);
             }
         }
+        //or
         System.out.println("Book ID looked for:"+this.countID+" ,the name of the book: "+this.names[this.countID]+" and it's type: "+this.types[this.countID]);
     }
 }
